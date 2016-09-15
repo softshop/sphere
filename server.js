382a1f4ded30c5f7b63b930eaa25ef8bb65668f4
Avi Kejariwal <avikejariwal@Avis-MacBook-Pro.local>
2016-09-15 17:51:56 +0530

    Configured sendgrid to send inquiries

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require('http').Server(app);
 var router = express.Router();
 var port = process.env.PORT || 8080;
 var bodyParser = require("body-parser");
+var sendgrid  = require('sendgrid')('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');
+var validator = require('validator');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +22,67 @@ router.get('/', function(req, res){
 	res.render('index');
 })
 
+router.post('/sendMail', function(req, res){
+	var emailTo = "avikejariwal@gmail.com";
+	var name = req.body.name;
+	var emailFrom = req.body.emailFrom;
+	var phoneNumber = req.body.phoneNumber;
+	var message = req.body.message;
+	var subject = 'Inquiry Through SphereAdvisory.com';
+
+	var messageString = "Name: " + name + '\n\n' + "Email: " + emailFrom + "\n\n" + "Phone Number: " + phoneNumber + "\n\n" + "Message: " + message;
+
+	console.log("--------------------------------");
+	console.log("Received Email Post...");
+	console.log("Name: " + name);
+	console.log("From: " + emailFrom);
+	console.log("Phone Number: " + phoneNumber);
+	console.log("Message: " + message);
+
+	var validName = (name != null);
+	var validEmail = validator.isEmail(emailFrom);
+	var validNumber = true;
+	if (phoneNumber != ""){
+		validNumber = validator.isNumeric(phoneNumber) && validator.isLength(phoneNumber, {min:10, max: 15});
+	}
+	var validMessage = (message != null);
+
+	if (!validName){
+		console.log("Invalid Name, sending error");
+		res.send({status: 400, error: "NAME"});
+	}
+	if (!validEmail){
+		console.log("Invalid Email, sending error");
+		res.send({status: 400, error: "EMAIL"});
+		return null;
+	}
+	if (!validNumber) {
+		console.log("Invalid Number, sending error");
+		res.send({status: 400, error: "PHONE_NUMBER"});
+		return null;
+	}
+	if (!validMessage) {
+		console.log("Invalid Message, sending error");
+		res.send({status: 400, error: "MESSAGE"});
+		return null;
+	}
+	console.log("Sending Email...");
+	sendgrid.send({
+		to: emailTo,
+		from: emailFrom,
+		subject: subject,
+		text: messageString
+	}, function(err, json){
+		if (err){
+			console.log("Error: " + err);
+			res.send({status:400, error: err});
+			return null;
+		}
+		console.log("Success!")
+		res.send({status: 200, error: "NONE"});
+	})
+})
+
 http.listen(port, function(){
-	console.log("listening on port: "+ port);
+	console.log("listening on port: " + port);
 });
--- static/js/index.js
+++ static/js/index.js
@@ -14,13 +14,60 @@
  		});
  	});
 
-  $(window).scroll(function() {
-  	if ($(window).scrollTop() + $(window).height() < 1400){
-  		$('nav').hide();
-  	} 
-  	else {
-  		$('nav').show();
-  	}
-});
+ 	$(window).scroll(function() {
+ 		if ($(window).scrollTop() + $(window).height() < 1400){
+ 			$('nav').hide();
+ 		} 
+ 		else {
+ 			$('nav').show();
+ 		}
+ 	});
 
  });
+
+ function sendMail(){
+ 	var name = $('#name').val();
+ 	var emailFrom = $('#emailFrom').val();
+ 	var phoneNumber = $('#phoneNumber').val();
+ 	var message = $('#message').val();
+ 	if (name == "") {
+ 		$('#name').addClass("error");
+ 		return null;
+ 	}
+ 	else {
+ 		$('#name').removeClass("error");
+ 	}
+ 	if (emailFrom == "" || !validator.isEmail(emailFrom)){
+ 		$('#emailFrom').addClass("error");
+ 		return null;
+ 	}
+ 	else {
+ 		$('#emailFrom').removeClass("error");
+ 	}
+ 	if (phoneNumber == "")
+ 		phoneNumber = null;
+ 	if (phoneNumber != null && (!validator.isNumeric(phoneNumber) || !validator.isLength(phoneNumber, {min:10, max:15}))){
+ 		$('#phoneNumber').addClass("error");
+ 		return null;
+ 	}
+ 	else {
+ 		$('#phoneNumber').removeClass("error");
+ 	}
+ 	if (message == ""){
+ 		$('#message').addClass("error");
+ 		return null;
+ 	}
+ 	else {
+ 		$('#message').removeClass("error");
+ 	}	
+ 	$.post('/sendMail', {
+ 		name: name,
+ 		emailFrom: emailFrom,
+ 		phoneNumber: phoneNumber,
+ 		message: message
+ 	}, function (response) {
+ 		alert("Inquiry Sent!");
+ 		$('#contactForm')[0].reset();
+ 		console.log(response);
+ 	})
+ }
